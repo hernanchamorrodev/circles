afbe61023dc28aa04fa9da4042bd2124facf212e
Hernan Chamorro <contacto@hernanchamorro.com>
2023-10-13 02:10:34 -0300

    header-design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,24 +4,146 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <div className="header__left">
-        {/* Logo */}
-        <img src="" alt="" />
-        {/* SearchIcon */}
-        <div className="header__search">
-          <input type="text" placeholder="Search circles" />
-        </div>
-      </div>
+    <div className="flex w-full border-gray-200 p-3 border-b">
+      <div className="container mx-auto w-full flex">
+        <div className="header__left w-1/2 flex items-center mx-6">
+          {/* Logo */}
+          <div className="header__logo flex items-center mr-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <h1 className="text-lg ml-1">Circles</h1>
+          </div>
+          {/* SearchIcon */}
+          <div className="header__search mr-5 w-full">
+            <input
+              className="border rounded-xl p-1 w-full"
+              type="text"
+              placeholder="Search circles"
+            />
+          </div>
+          <div className="activity-area flex gap-5 ">
+            {/* HomeIcon */}
+            <div className="home">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="text-gray-500 mx-auto w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
 
-      <div className="header__right">
-        {/* HomeIcon */}
-        {/* PeopleIcon */}
-        {/* WorkIcon */}
-        {/* ChatIcon */}
-        {/* NotificationIcon */}
-        {/* Avatar for logged in user */}
-        {/* Drowdown */}
+              <p className="text-gray-500">Home</p>
+            </div>
+
+            {/* PeopleIcon */}
+            <div className="people">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="text-gray-500 w-6 h-6 mx-auto"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                />
+              </svg>
+
+              <p className="text-gray-500">People</p>
+            </div>
+            {/* JobsIcon */}
+            <div className="jobs">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="text-gray-500 mx-auto w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+                />
+              </svg>
+              <p className="text-gray-500">Jobs</p>
+            </div>
+            {/* MessagesIcon */}
+            <div className="messages">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="text-gray-500 mx-auto w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                />
+              </svg>
+              <p className="text-gray-500">Messages</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="header__right w-1/2 flex justify-evenly">
+          {/* User Area */}
+          <div className="user-area flex items-center gap-3">
+            {/* Avatar */}
+            <div className="avatar">
+              <img
+                className="rounded-full w-10 h-10"
+                src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="avatar"
+              />
+            </div>
+            {/* NotificationIcon */}
+            <div className="notifications">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+            </div>
+          </div>
+
+          {/* Drowdown */}
+        </div>
       </div>
     </div>
   );
